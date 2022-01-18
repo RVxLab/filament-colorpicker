3b47855e9595d4e554c4a651c388537024db0f3f
RVxLab <richard@rvx.works>
2022-01-18 12:36:09 +1100

    Merge FilamentPluginServiceProvider into FilamentColorPickerServiceProvider

--- src/FilamentColorPickerServiceProvider.php
+++ src/FilamentColorPickerServiceProvider.php
@@ -4,20 +4,24 @@ declare(strict_types=1);
 
 namespace RVxLab\FilamentColorPicker;
 
+use Filament\PluginServiceProvider;
 use Spatie\LaravelPackageTools\Package;
-use Spatie\LaravelPackageTools\PackageServiceProvider;
 
-final class FilamentColorPickerServiceProvider extends PackageServiceProvider
+final class FilamentColorPickerServiceProvider extends PluginServiceProvider
 {
+    public static string $name = 'Colorpicker';
+
+    /**
+     * @var string[]
+     */
+    protected array $scripts = [
+        'rvxlab-filament-colorpicker' => __DIR__ . '/../resources/dist/filament-colorpicker.js',
+    ];
+
     public function configurePackage(Package $package): void
     {
         $package
             ->name('filament-colorpicker')
             ->hasViews();
-    }
-
-    public function packageRegistered(): void
-    {
-        $this->app->register(FilamentPluginServiceProvider::class);
     }
 }
