fe0496d45f2b7e4cfbb504c7a1819eaf9d0decef
Morgan Chorlton <morganchorlton3@gmail.com>
2022-01-26 22:02:40 +0000

    Added Color Swatch Column

---
+++ resources/views/colorswatch.blade.php
@@ -0,0 +1,3 @@
+<div style="background-color: {{ $getState() }}; width: 3rem; height: 1.75rem; margin-left: 1rem; border-radius: 10px;">
+</div>
+
---
+++ src/Columns/ColorSwatch.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RVxLab\FilamentColorPicker\Columns;
+
+use Filament\Tables\Columns\Column;
+
+class ColorSwatch extends Column
+{
+    protected string $view = 'filament-colorpicker::colorSwatch';
+}
