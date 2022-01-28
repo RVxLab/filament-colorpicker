cb7e352b445c9986adb8f397d62a5518ed30787c
RVxLab <richard@rvx.works>
2022-01-28 21:08:40 +1000

    Add the ability to copy the color on the swatch

--- resources/views/color-swatch.blade.php
+++ resources/views/color-swatch.blade.php
@@ -1,2 +1,40 @@
-<div style="background-color: {{ $getState() }}; width: 1.5rem; height: 1.5rem; margin-left: 1rem; border-radius: 6px;">
-</div>
+@php
+$copyable = $isCopyable();
+$tag = $copyable ? 'button' : 'div';
+@endphp
+
+@if ($copyable)
+    <button
+        x-data="{
+            showCopied: false,
+            copiedMessageTimeout: -1,
+            copy() {
+                window
+                    .navigator
+                    .clipboard
+                    .writeText('{{ $getState() }}')
+                    .then(() => {
+                        this.showCopied = true;
+
+                        window.clearTimeout(this.copiedMessageTimeout);
+
+                        this.copiedMessageTimeout = window.setTimeout(() => {
+                            this.showCopied = false;
+                        }, {{ $getCopyMessageShowTimeMs() }});
+                    });
+            },
+        }"
+        @click.prevent.stop="copy"
+@endif
+    <span
+        class="relative flex"
+        style="background-color: {{ $getState() }}; width: 1.5rem; height: 1.5rem; margin-left: 1rem; border-radius: 6px;"
+        title="{{ $getState() }}"
+    >
+    </span>
+@if ($copyable)
+        <span x-cloak x-show="showCopied" class="absolute pointer-events-none top-0" style="left: 2rem">
+            {{ $getCopyMessage() }}
+        </span>
+    </button>
+@endif
--- src/Columns/ColorSwatch.php
+++ src/Columns/ColorSwatch.php
@@ -9,4 +9,46 @@ use Filament\Tables\Columns\Column;
 class ColorSwatch extends Column
 {
     protected string $view = 'filament-colorpicker::color-swatch';
+
+    protected bool $copyable = false;
+
+    protected string $copyMessage = 'Copied!';
+
+    protected int $copyMessageShowTimeMs = 2000;
+
+    public function copyable(): self
+    {
+        $this->copyable = true;
+
+        return $this;
+    }
+
+    public function copyMessage(string $message): self
+    {
+        $this->copyMessage = $message;
+
+        return $this;
+    }
+
+    public function getCopyMessage(): string
+    {
+        return $this->copyMessage;
+    }
+
+    public function copyMessageShowTimeMs(int $timeInMs): self
+    {
+        $this->copyMessageShowTimeMs = $timeInMs;
+
+        return $this;
+    }
+
+    public function getCopyMessageShowTimeMs(): int
+    {
+        return $this->copyMessageShowTimeMs;
+    }
+
+    public function isCopyable(): bool
+    {
+        return $this->copyable;
+    }
 }
